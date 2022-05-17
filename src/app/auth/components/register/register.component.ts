1d3e55787087817dbb092b354a9f38b331e3a077
Лейсан Валиуллина <Leisan3517@mail.ru>
2022-05-17 12:00:35 +0200

    Installed NgRx

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core'
+import {FormGroup, FormBuilder, Validators} from '@angular/forms'
+import {Store} from "@ngrx/store";
+import { registerAction } from 'src/app/auth/store/actions/register.action'
+
+@Component({
+  selector: 'iula-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+
+export class RegisterComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder, private store: Store) {
+  }
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  initializeForm(): void {
+    console.log('initializeForm')
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+  onSubmit(): void {
+    console.log('submit', this.form.value, this.form.valid)
+    this.store.dispatch(registerAction(this.form.value))
+  }
+}
