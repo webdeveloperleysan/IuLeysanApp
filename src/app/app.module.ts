9c6bb853342411caa6e9da23448e4eeaaeb87769
Лейсан Валиуллина <Leisan3517@mail.ru>
2022-05-24 12:07:52 +0200

    creating auth interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AppComponent } from 'src/app/app.component';
+import { AuthModule } from 'src/app/auth/auth.module';
+import {StoreModule} from "@ngrx/store";
+import {environment} from "src/environments/environment";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/store/actions/register.action.ts
+++ src/app/auth/store/actions/register.action.ts
@@ -1,7 +1,8 @@
 import {createAction, props} from '@ngrx/store'
 import {ActionTypes} from 'src/app/auth/store/actionTypes'
+import {RegisterRequestInterface} from "../../types/registerRequest.inteface";
 
 export const registerAction = createAction(
   ActionTypes.REGISTER,
-  props<{username: string; email: string; password: string }>()
+  props<RegisterRequestInterface>()
 )
