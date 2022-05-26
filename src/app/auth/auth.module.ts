d0795324ac64cc8d943cc0dcf3c73ba819d856f1
Лейсан Валиуллина <Leisan3517@mail.ru>
2022-05-26 14:46:05 +0200

    adding auth reducer

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,8 @@ import {CommonModule} from "@angular/common"
 import {RegisterComponent} from 'src/app/auth/components/register/register.component'
 import {Routes, RouterModule} from "@angular/router"
 import {ReactiveFormsModule} from '@angular/forms'
+import {StoreModule} from "@ngrx/store";
+import {reducers} from "./store/reducers";
 
 const routes: Routes = [
   {
@@ -13,7 +15,12 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [CommonModule,  RouterModule.forChild(routes), ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducers)
+  ],
   declarations: [RegisterComponent]
 })
 export class AuthModule {}
